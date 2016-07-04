90d97d908af75f67048c2deb71b0f0b0a2149f5e
juanjocruz <juanjojac@gmail.com>
2016-07-04 14:29:56 +0200

    proyecto inicial

---
+++ app/src/main/java/com/example/dell_09_027_pc/proyectomuseos/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.dell_09_027_pc.proyectomuseos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Item> category = new ArrayList<Item>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<Lista_entrada> datos = new ArrayList<Lista_entrada>();
+
+        datos.add(new Lista_entrada(R.drawable.casa_de_Jovellanos, "Museo Jovellanos", "Casa natal de Jovellanos"));
+        datos.add(new Lista_entrada(R.drawable.ferrocarril, "Museo del ferrocarril", "Museo del ferrocarril"));
+        datos.add(new Lista_entrada(R.drawable., "Museo Piñole", "Pinoteca en la casona natal de Piñole"));
+
+
+
+
+
+    }
+}
