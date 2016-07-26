7c9c329d176b14f1513b724d45354ca9015a9e4c
naty1973 <npsf1973@gmail.com>
2016-07-26 13:44:01 +0200

    creado Listado Nombres Activity (java y layout), pueto el id en el xml y borrado ficha 3

---
+++ app/src/main/java/com/example/dell_09_027_pc/proyectomuseos/ListadoNombresActivity.java
@@ -0,0 +1,13 @@
+package com.example.dell_09_027_pc.proyectomuseos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListadoNombresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_nombres);
+    }
+}
